ab87b4c7f3fc2cebaea430cfc4b5944438f9a4c1
Lukasz Siwek <xofczarx@gmail.com>
2017-05-17 09:09:53 +0200

    Get-MyFile was fixed

--- module5/Get-MyFile.ps1
+++ module5/Get-MyFile.ps1
@@ -1,30 +1,29 @@
-param([string[]]$ComputerName = 'localhost', [string]$Criteria, [hashtable]$Attributes)
+param ([string[]]$Computername = 'localhost', [string]$Criteria, [hashtable]$Attributes)
 
-foreach($Computer in $ComputerName) {
-    $CimInstParams = @{'ClassName' = 'Win_Share'}
-    if($Computer -ne 'localhost') {
-        $CimInstParams.ComputerName = $Computer
+foreach ($Computer in $Computername) {
+	## Enumerate all of the default admin shares
+    $CimInstParams = @{'ClassName' = 'Win32_Share'}
+    if ($Computer -ne 'localhost') {
+	    $CimInstParams.Computername = $Computer    
     }
-    $DriveShares = (Get-CimInstance $CimInstParams | Where-Object { $_.Name -match '^[A-Z]\$$'}).Name
-
-    foreach($Drive in $DriveShares)
-    {
-        switch ($Criteria) {
-            'Extension' { 
+    $DriveShares = (Get-CimInstance @CimInstParams | where { $_.Name -match '^[A-Z]\$$' }).Name
+	foreach ($Drive in $DriveShares) {
+		switch ($Criteria) {
+			'Extension' {
                 Get-ChildItem -Path "\\$Computer\$Drive" -Filter "*.$($Attributes.Extension)" -Recurse
-             }
-             'Age' {
-                 $Today = Get-Date
-                 $DaysOld = $Attributes.DaysOld
-                  Get-ChildItem -Path "\\$Computer\$Drive" -Recurse | Where-Object { $_.LastWriteTime -le $Today.AddDays(-$DaysOld)}
-             }
-             'Name' {
-                 $Name = $Attributes.Name
-                  Get-ChildItem -Path "\\$Computer\$Drive" -Filter "*$Name*" -Recurse
-             }
-            Default {
+			}
+			'Age' {
+				$Today = Get-Date
+                $DaysOld = $Attributes.DaysOld
+                Get-ChildItem -Path "\\$Computer\$Drive" -Recurse | Where-Object { $_.LastWriteTime -le $Today.AddDays(-$DaysOld)}
+			}
+			'Name' {
+                $Name = $Attributes.Name
+				Get-ChildItem -Path "\\$Computer\$Drive" -Filter "*$Name*" -Recurse
+			}
+            default {
                 Write-Error "Unrecognized criteria '$Criteria'"
             }
-        }
-    }
+		}
+	}
 }
